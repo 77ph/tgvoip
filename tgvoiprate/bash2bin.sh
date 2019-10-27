2b659782076f62f4241dec41bdeba6b11e9917a6
77ph <33835429+77ph@users.noreply.github.com>
2019-10-27 13:46:48 +0300

    Create bash2bin.sh

---
+++ tgvoiprate/bash2bin.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ ! -f /usr/local/bin/shc ]; then  
+git clone https://github.com/neurobin/shc.git
+cd shc/
+./configure
+make
+sudo make install
+cd ..
+rm -rf shc
+fi
+
+/usr/local/bin/shc -f ./tgvoiprate.sh -o ./tgvoiprate
+
+
+
